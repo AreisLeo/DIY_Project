28ad6d4e20ed9fcf87c322af9bd7fe7505e59ed2
AreisLeo <361467298@qq.com>
2015-06-16 15:44:33 +0800

    222222
    修改123123

--- DIY_doYourself/DIY_doYourself/AppDelegate.m
+++ DIY_doYourself/DIY_doYourself/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // Override point for 123123 after application launch.
     return YES;
 }
 
