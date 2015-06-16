32f0fec7d5409d46f7b51abe20d60522c7ab5404
gjj <717764882@qq.com>
2015-06-16 14:41:14 +0300

    初始化我们的项目

---
+++ DIY_doYourself/DIY_doYourself/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DIY_doYourself
+//
+//  Created by JJetGu on 15-6-16.
+//  Copyright (c) 2015年 Free. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
